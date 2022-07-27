a1939a146a2aebcdb957136a6f074a4be723078a
Darryl Brooks <darrylbrooks13@gmail.com>
2022-07-27 17:15:04 -0500

    chore: minor updates

--- clients/supabase.ts
+++ clients/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "supabase";
-import { config } from "config";
+import {configSync} from "config";
 
-const url: string = config().SB_URL;
-const key: string = config().SB_ANON_KEY;
+const url: string = Deno.env.get("SB_URL") || configSync().SB_URL;
+const key: string = Deno.env.get("SB_ANON_KEY") || configSync().SB_ANON_KEY;
 
 export const supabase = createClient(url, key);
