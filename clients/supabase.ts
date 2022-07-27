1ebb7a533bf62143ab71ccca6ac40dd9ac3aca21
Darryl Brooks <darrylbrooks13@gmail.com>
2022-07-27 14:26:08 -0500

    feat: introducing supabase

---
+++ clients/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from "supabase"
+import { config } from "config"
+
+const url: string = config().SB_URL;
+const key: string = config().SB_ANON_KEY;
+
+export const supabase = createClient(url,key);
