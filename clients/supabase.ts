e0d142aaee6e80745faed8fb0d7ce4e4891c72d5
Darryl Brooks <darrylbrooks13@gmail.com>
2022-07-28 00:47:23 -0500

    refactor: formatting

--- clients/supabase.ts
+++ clients/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "supabase";
-import {configSync} from "config";
+import { configSync } from "config";
 
 const url: string = Deno.env.get("SB_URL") || configSync().SB_URL;
 const key: string = Deno.env.get("SB_ANON_KEY") || configSync().SB_ANON_KEY;
