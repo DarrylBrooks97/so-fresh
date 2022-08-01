7c4eb1f95e16bb127a212c0ceff446ac6fc628d5
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-01 16:08:42 -0500

    feat: changing signin to normal header

---
+++ islands/Header.tsx
@@ -0,0 +1,18 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+export default function Header() {
+  return (
+      <div class={tw`sticky top-0 mx-auto max-w-7xl items-center z-90`}>
+        <div class={tw`w-max cursor-pointer`} onClick={()=> window.location.href = ''}>
+          <p
+            class={tw
+              `text-3xl bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent`}
+          >
+            So-Fresh
+          </p>
+        </div>
+      </div>
+  );
+}
