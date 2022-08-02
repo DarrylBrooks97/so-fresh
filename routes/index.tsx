abac9c7aa873e2ef449517d7d563b06d90449f84
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-01 22:29:52 -0500

    chore: minor updates

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,31 +11,38 @@ export const handler: Handlers = {
 };
 const SideBarLayout = (props: { children: any }) => {
   return (
-    <div class={tw`flex`}>
+    <div class={tw`flex max-w-7xl mx-auto py-3`}>
       <div
-        class={tw
-          `w-max min-h-screen flex flex-col shadow-white shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)]`}
+        class={tw`w-max min-h-screen flex flex-col shadow-xl shadow-red-200`}
       >
         <input class={tw`bg-transparent border-white border-2 text-white`} />
         <div class={tw`p-3 flex items-center justify-center`}>
-          <p class={tw`text-xl text-white`}>
+          <p class={tw`text-xl text-white text-center w-full h-[100px]`}>
             Saved
           </p>
         </div>
       </div>
-      <div class={tw`p-3 grow w-full`}>
+      <div class={tw`grow`}>
         {props.children}
       </div>
     </div>
   );
 };
-
+const MovieTopics = ["Action", "Drama", "Comedy", "Horror"];
 export default function Home() {
   return (
-    <div class={tw`bg-black min-h-screen min-w-screen p-6`}>
+    <div class={tw`bg-black h-full w-full p-6`}>
       <Header />
       <SideBarLayout>
-        <p class={tw`text-white`}>Hello World</p>
+        <div class={tw`flex pl-3 justify-center`}>
+          {MovieTopics.map((topic) => (
+            <div class={tw`pl-3`}>
+              <p class={tw`text-xl text-white cursor-pointer`}>
+                {topic}
+              </p>
+            </div>
+          ))}
+        </div>
       </SideBarLayout>
     </div>
   );
