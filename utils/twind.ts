3c0732561c6fe422852cb9387280dba53463bf7f
Darryl Brooks <darrylbrooks13@gmail.com>
2022-08-12 21:45:54 -0500

    chore: showing twind warnings

--- utils/twind.ts
+++ utils/twind.ts
@@ -1,8 +1,9 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Configuration, setup } from "twind";
 export * from "twind";
+
 export const config: Configuration = {
   darkMode: "class",
-  mode: "silent",
+  mode: "warn",
 };
 if (IS_BROWSER) setup(config);
